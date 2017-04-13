c8f1e996ceaa9ae879de610510f6d44a253d373b
Jonathan Brown <jbrown@bluedroplet.com>
2017-04-13 16:13:05 +0700

    Update smart contract.

--- link_revenue.sol
+++ link_revenue.sol
@@ -11,27 +11,59 @@ contract LinkRevenue {
     uint public withdrawn;
     address public owner;
 
-    function LinkRevenue() {
-        startTime = block.timestamp;
-        owner = msg.sender;
-    }
+    /**
+     * @dev The owner of this contract has chanaged.
+     * @param oldOwner Old owner of this contract.
+     * @param newOwner New Owner of this contract.
+     */
+    event ChangeOwner(address oldOwner, address newOwner);
 
+    /**
+     * @dev A withdrawal has occured.
+     * @param recipient Recipient of the withdrawal.
+     * @param amount Amount of Link withdrawn.
+     */
+    event Withdraw(address recipient, uint amount);
+
+    /**
+     * @dev Throws if the sender is not the owner of the contract.
+     */
     modifier isOwner() {
         require (msg.sender == owner);
         _;
     }
 
-    function changeOwner(address newOwner) external isOwner {
-        owner = newOwner;
+    /**
+     * @dev Constructor.
+     */
+    function LinkRevenue() {
+        startTime = block.timestamp;
+        owner = msg.sender;
     }
 
+    /**
+     * @dev Changes the owner of the contract.
+     * @param newOwner The new owner of the contract.
+     */
+    function changeOwner(address newOwner) external isOwner {
+        owner = newOwner;
+        ChangeOwner(msg.sender, newOwner);
+    }
+
+    /**
+     * @dev Withdraws any available funds to the owner.
+     */
     function withdraw() external isOwner {
         uint released = getReleased();
         uint amount = released - withdrawn;
         withdrawn = released;
         owner.transfer(amount);
+        Withdraw(owner, amount);
     }
 
+    /**
+     * @dev Determines how much Link has been released in total so far.
+     */
     function getReleased() constant returns (uint released) {
         uint dailyAmount = 50000 ether;
         int elapsed = int((block.timestamp - startTime) / 1 days);
