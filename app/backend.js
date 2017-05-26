a4d36357c07124fc3ba18cf3b276f3a20f234760
Jonathan Brown <jbrown@link-blockchain.org>
2017-05-26 15:54:50 +0700

    Change port to 3001 and use Singapore bootnode for RPC.

--- app/backend.js
+++ app/backend.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Link Blockchain Revenue Status app listening on port 3000!');
+app.listen(3001, function () {
+  console.log('Link Blockchain Revenue Status app listening on port 3001!');
 })
